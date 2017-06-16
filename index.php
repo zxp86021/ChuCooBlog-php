dc74be9967d6329d7675aaafd787e174c45d6ca9
richegg <zxp86021@gmail.com>
2017-06-16 23:08:23 +0800

    update cors header setting

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Method: *');
 header('Access-Control-Allow-Headers: *');
 
